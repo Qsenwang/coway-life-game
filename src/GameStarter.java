89a8df8719fcede93d5de7e810afda0c4c0d6a5c
quansen <qsen.wang@gmail.com>
2021-11-05 14:14:03 +1000

    init origin state and tested.

--- src/GameStarter.java
+++ src/GameStarter.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+
 public class GameStarter {
     // main entry
     public static void main(String[] args) {
+        //build a class can store multi pair int values( duplication available)
+        ArrayList<AliveCell> aliveCellsSet = new ArrayList<>();
+        // add the active cell point. for instance: [5,5] [5,6] [6,5] [6,6] [7,5] [7,6]
+        aliveCellsSet.add(new AliveCell(5, 5));
+        aliveCellsSet.add(new AliveCell(6, 5));
+        aliveCellsSet.add(new AliveCell(7, 5));
+        aliveCellsSet.add(new AliveCell(5, 6));
+        aliveCellsSet.add(new AliveCell(6, 6));
+        aliveCellsSet.add(new AliveCell(7, 6));
 
+        //generate 100 round state changing
+        State state = new State();
+        state.setInitState(aliveCellsSet);
+        state.showState();
     }
 }
--- src/State.java
+++ src/State.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class State {
     static final int ROWLEN=200;
     static final int COLLEN=200;
@@ -6,9 +8,33 @@ public class State {
     int[][] preState = new int[ROWLEN][COLLEN];
     int[][] tempState = new int[ROWLEN][COLLEN];
 
+    //Set prestate to initstate according to the pairs of cell position.
+    public int[][] setInitState(ArrayList<AliveCell> aliveCellsSet) {
+        for (int i = 0; i < ROWLEN; i++) {
+            for (int j = 0; j < COLLEN; j++) {
+                preState[i][j] = DEAD;
+            }
+        }
+        for (AliveCell aliveCell : aliveCellsSet) {
+            preState[aliveCell.getX()][aliveCell.getY()] = ALIVE;
+        }
+        return preState;
+    }
 
-    public  void setInitstate(){};
+
     public void aroundActiveCellSum(int row, int col) {}
     public void nextState(){};
-    public void showState(){};
+
+
+    //Print the state set, output the active cell's position index;
+    public void showState(){
+        for (int row = 0; row < ROWLEN; row++) {
+            for (int col = 0; col < COLLEN; col++) {
+                if (preState[row][col] == ALIVE) {
+                    System.out.printf("[" + row + "][" + col + "] ");
+                }
+            }
+        }
+        System.out.println("");
+    };
 }
