696a62020517cf9f1a6f9cd819af23668f60b8d9
quansen <qsen.wang@gmail.com>
2021-11-05 14:26:20 +1000

    build aroundcheck funtion to sum the around active cells and be used in the function next state tp generate next state of the cell metrix

--- src/GameStarter.java
+++ src/GameStarter.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 public class GameStarter {
     // main entry
     public static void main(String[] args) {
-        //build a class can store multi pair int values( duplication available)
+        // Build a class can store multi int values pairs (duplication allowed)
         ArrayList<AliveCell> aliveCellsSet = new ArrayList<>();
-        // add the active cell point. for instance: [5,5] [5,6] [6,5] [6,6] [7,5] [7,6]
+        // Add the active cell point. for instance: [5,5] [5,6] [6,5] [6,6] [7,5] [7,6]
         aliveCellsSet.add(new AliveCell(5, 5));
         aliveCellsSet.add(new AliveCell(6, 5));
         aliveCellsSet.add(new AliveCell(7, 5));
@@ -13,9 +13,13 @@ public class GameStarter {
         aliveCellsSet.add(new AliveCell(6, 6));
         aliveCellsSet.add(new AliveCell(7, 6));
 
-        //generate 100 round state changing
+        //generate 100 round cells change state
         State state = new State();
         state.setInitState(aliveCellsSet);
-        state.showState();
+        for (int i = 0; i < 100; i++) {
+            System.out.printf("the "+(i+1)+ " state out put is: ");
+            state.showState();
+            state.nextState();
+        }
     }
 }
--- src/State.java
+++ src/State.java
@@ -8,7 +8,7 @@ public class State {
     int[][] preState = new int[ROWLEN][COLLEN];
     int[][] tempState = new int[ROWLEN][COLLEN];
 
-    //Set prestate to initstate according to the pairs of cell position.
+    //Set prestate to initstate according to the pairs of cells position.
     public int[][] setInitState(ArrayList<AliveCell> aliveCellsSet) {
         for (int i = 0; i < ROWLEN; i++) {
             for (int j = 0; j < COLLEN; j++) {
@@ -22,8 +22,54 @@ public class State {
     }
 
 
-    public void aroundActiveCellSum(int row, int col) {}
-    public void nextState(){};
+    //Sum up all the around actived cells in preState. (let-top, top, right-top, let, mid, right,let-bot, bot, right-bot)
+    public int aroundActiveCellSum(int row, int col) {
+        int sum = 0;
+        int r, c;
+        for (r = row - 1; r <= row + 1; r++) {
+            for (c = col - 1; c <= col + 1; c++) {
+                // border situcation
+                if (r < 0 || r >= ROWLEN || c < 0 || c >= COLLEN) {
+                    continue;
+                }
+                if (preState[r][c] == ALIVE) {
+                    sum++;
+                }
+            }
+        }
+        if (preState[row][col] == ALIVE) {
+            sum--;
+        }
+        return sum;
+    }
+
+
+    // Using aoroundActiveCellSum function check each cell condition
+    // and according to the game rule record the cells states into the tempState set.
+    // then Overwriting the preState with temState, which means after the overwriting,
+    // preSate now is the next state of the previous preState
+    public int[][] nextState() {
+        for (int row = 0; row < ROWLEN; row++) {
+            for (int col = 0; col < COLLEN; col++) {
+                switch (aroundActiveCellSum(row, col)) {
+                    case 2:
+                        tempState[row][col] = preState[row][col];
+                        break;
+                    case 3:
+                        tempState[row][col] = ALIVE;
+                        break;
+                    default:
+                        tempState[row][col] = DEAD;
+                }
+            }
+        }
+        for (int row = 0; row < ROWLEN; row++) {
+            for (int col = 0; col < COLLEN; col++) {
+                preState[row][col] = tempState[row][col];
+            }
+        }
+        return preState;
+    }
 
 
     //Print the state set, output the active cell's position index;
