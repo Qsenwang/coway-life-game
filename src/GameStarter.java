5a76d98cb0bdd8517abe60b00258d7f23ff07c78
quansen <qsen.wang@gmail.com>
2021-11-05 14:28:30 +1000

    add a comment for how to useing the game starter, and all the project are finished

--- src/GameStarter.java
+++ src/GameStarter.java
@@ -13,7 +13,8 @@ public class GameStarter {
         aliveCellsSet.add(new AliveCell(6, 6));
         aliveCellsSet.add(new AliveCell(7, 6));
 
-        //generate 100 round cells change state
+        //generate 100 round cells change state,
+        // the number in the for loop is used to control how many rounds you want
         State state = new State();
         state.setInitState(aliveCellsSet);
         for (int i = 0; i < 100; i++) {
